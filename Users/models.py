fe06226c88098f5ca61dfbecd2d9e586571d1e34
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2023-04-24 18:38:08 +0300

    analytics app

--- Users/models.py
+++ Users/models.py
@@ -4,10 +4,15 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+def csv_upload_to(instance, filename):
+    """Return the path where the profile picture file should be uploaded"""
+    return f"profile_pictures/{instance.user.username}/{filename}"
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     User._meta.get_field('email')._unique = True
-    profile_pic = models.ImageField(upload_to='profile_pictures', blank=True)
+    profile_pic = models.ImageField(upload_to=csv_upload_to)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
@@ -18,4 +23,3 @@ class UserProfile(models.Model):
             return self.profile_pic.url
         else:
             return '/static/images/default_profile_pic.png'
-
