0d6caebb32291c9279b092ba4348abedbbd30586
Alex <98416718+alexandru-ghibea@users.noreply.github.com>
2023-05-15 17:46:35 +0300

    fixed plot for profile analytics

--- analytics/views.py
+++ analytics/views.py
@@ -2,8 +2,6 @@ import base64
 from django.views.generic import TemplateView
 from uploads.models import Csv
 import pandas as pd
-from django.views.generic import TemplateView
-import numpy as np
 from django.http import HttpResponse
 import io
 import matplotlib.pyplot as plt
@@ -19,13 +17,15 @@ class CsvAnalyticsView(TemplateView):
         user = self.request.user
         filename = self.kwargs.get('filename')
         profile = self.kwargs.get('profile')
-        paginate_by = 5
+        plot_data1 = None
+        plot_data = None
+        plot_data2 = None
+        plot_title = None
 
         if filename == 'Profiles.csv':
             selected_columns = [
                 'Profile Name', 'Profile Creation Time', 'Maturity Level', 'Primary Lang']
-            plot_title = None
-            plot_data = None
+
         elif filename == 'BillingHistory.csv':
             selected_columns = ['Transaction Date', 'Payment Type',
                                 'Country', 'Currency', 'Gross Sale Amt', 'Pmt Status']
@@ -69,13 +69,12 @@ class CsvAnalyticsView(TemplateView):
             plot_data = HttpResponse(buf.getvalue(), content_type='image/png')
 
             # Add the plot to the context as a base64-encoded string
-            plot_data = base64.b64encode(buf.getvalue()).decode('utf-8')
+            plot_data = base64.b64encode(
+                buf.getvalue()).decode('utf-8')
 
         elif filename == 'MyList.csv':
             selected_columns = [
                 "Profile Name",	"Title Name", "Utc Title Add Date"]
-            plot_data = None
-            plot_title = None
             csv_file_obj = Csv.objects.filter(
                 user=user, csv_file__icontains=filename).first()
             csv_file_path = csv_file_obj.csv_file.path
@@ -90,8 +89,47 @@ class CsvAnalyticsView(TemplateView):
                 'pivot_dict': pivot_dict,
                 'selected_profile': profile,
             }
+        elif filename == "ViewingActivity.csv":
+            selected_columns = [
+                "Profile Name", "Start Time", "Duration", "Title", "Device Type"]
+            csv_file_obj = Csv.objects.filter(
+                user=user, csv_file__icontains=filename).first()
+            csv_file_path = csv_file_obj.csv_file.path
+            df = pd.read_csv(csv_file_path, usecols=selected_columns)
+            df['Duration'] = pd.to_timedelta(df['Duration'])
+
+            # Group the data by Profile Name and calculate total duration for each profile
+            duration_data = df.groupby('Profile Name')['Duration'].sum()
+
+            # Create pie chart for total duration by profile
+            fig, ax = plt.subplots()
+            formatted_durations = duration_data.apply(
+                lambda x: f"{x.days} days\n{x.components.hours} hrs and {x.components.minutes} minutes")
+            labels = [f'{label} ({duration})' for label, duration in zip(
+                duration_data.index, formatted_durations)]
+            ax.pie(duration_data, labels=labels, autopct='%0.2f%%')
+
+            # ax.pie(duration_data, labels=labels, autopct='%1.1f%%')
+
+            plt.title('Duration by Profile')
+            plt.axis('equal')
+
+            # Save the plot to a buffer
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+
+            # Add the plot to the context as an HTTP response
+            plot_data = HttpResponse(buf.getvalue(), content_type='image/png')
+
+            # Add the plot to the context as a base64-encoded string
+            plot_data = base64.b64encode(buf.getvalue()).decode('utf-8')
+
+            # Clear the plot
+            plt.close(fig)
+
         else:
-            context['error_message'] = 'Not implemented yet'
+            context['error_message'] = 'View not implemented yet for this file.'
             context['plot_message'] = 'Plot not available for this file yet.'
             return context
 
@@ -106,4 +144,6 @@ class CsvAnalyticsView(TemplateView):
         context['table_data'] = table_data
         context['plot_title'] = plot_title
         context['plot_data'] = plot_data
+        context['plot_data1'] = plot_data1
+        context['plot_data2'] = plot_data2
         return context
